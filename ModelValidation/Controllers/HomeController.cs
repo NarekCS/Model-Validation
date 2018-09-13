d3caf2601d108c30d4115c7211e69942e3970e4b
narek.avanesyan <narek.avanesyan@FC-PROG-16>
2018-09-13 18:21:40 +0400

    Model Validation

---
+++ ModelValidation/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ModelValidation.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ModelValidation.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View("MakeBooking", new Appointment { Date = DateTime.Now });
+
+        [HttpPost]
+        public ViewResult MakeBooking(Appointment appt) => View("Completed", appt);
+
+    }
+}
